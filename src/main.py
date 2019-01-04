fc5ac863815b8fe3b8bbf16cb1580f17afea3ad8
Seseel Lybon <Seseellybon@gmail.com>
2019-01-04 14:30:07 +0100

    Started first phase for text.

---
+++ src/main.py
@@ -0,0 +1,62 @@
+import sys, pygame
+import time
+import math
+import pygame.freetype
+
+print("Hello world")
+
+pygame.init()
+
+size = width, height = 900, 600
+
+pos = [300, 300]
+
+screen = pygame.display.set_mode(size)
+
+
+black = 0, 0, 0
+time_frame = 30
+
+time_next = 0
+
+GAME_FONT  = pygame.freetype.SysFont("Arial",24)
+text_surface, rect = GAME_FONT.render("Hello World!", (255, 255, 255))
+
+running = True
+
+while running:
+    time_next = time.time() + 1 / time_frame
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            running = False
+            break
+
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_1:
+                text_surface, rect = GAME_FONT.render("1111111111", (255, 255, 255))
+            if event.key == pygame.K_2:
+                text_surface, rect = GAME_FONT.render("222222222", (255, 255, 255))
+            if event.key == pygame.K_3:
+                text_surface, rect = GAME_FONT.render("333333333", (255, 255, 255))
+            if event.key == pygame.K_4:
+                text_surface, rect = GAME_FONT.render("44444444444", (255, 255, 255))
+
+    #Do things here
+
+    screen.fill(black)
+    screen.blit(text_surface, (100, 100))
+    pygame.display.flip()
+
+    time_dif = time_next - time.time()
+    if time_dif > 0:
+        time.sleep(time_dif)
+        # print("sleeping for:", time_dif)
+
+
+
+
+print("Reached end of main.py")
+
